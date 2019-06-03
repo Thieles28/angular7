8f3839dfc1ded9de9f9ee48252592cb65cd9b67c
rthieles <rthieles@stj.jus.br>
2019-06-03 11:36:43 -0300

    atualização

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -1,16 +1,15 @@
-/* tslint:disable:no-unused-variable */
+import { TestBed, inject } from '@angular/core/testing';
 
-import { TestBed, async, inject } from '@angular/core/testing';
 import { MessageService } from './message.service';
 
-describe('Service: Message', () => {
+describe('MessageService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [MessageService]
     });
   });
 
-  it('should ...', inject([MessageService], (service: MessageService) => {
+  it('should be created', inject([MessageService], (service: MessageService) => {
     expect(service).toBeTruthy();
   }));
 });
